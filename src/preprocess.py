36366c3f05e38e27e37b24fa1983747edca7f281
Faisasa12 <faisalmufadhi@gmail.com>
2025-10-22 21:42:04 +0100

    Added preprocess python file

---
+++ src/preprocess.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+
+def train_test_from_path(csv_path):
+    dataset = pd.read_csv(csv_path)
+    
+    dataset.columns = [col.replace(".",'_') for col in dataset.columns]
+    dataset = dataset.drop('fnlwgt', axis=1)
+    dataset.replace('?', np.nan, inplace=True)
+    
+    dataset.dropna(inplace=True)
+    
+    features = dataset.drop('income', axis=1)
+    target = dataset['income']
+    
+    features = pd.get_dummies(features)
+    target = target.map({
+        '<=50K': 0,
+        '>50K': 1
+    })
+    
+    return train_test_split(features, target, test_size = 0.2, stratify = target)
