1719ec4161cf2975dae3967e1eaa185ea0e9d745
Faisasa12 <faisalmufadhi@gmail.com>
2025-10-22 22:04:44 +0100

    Added processing dataset to preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -23,3 +23,21 @@ def train_test_from_path(csv_path):
     })
     
     return train_test_split(features, target, test_size = 0.2, stratify = target)
+
+def train_test_from_raw_data(dataset):
+    dataset.columns = [col.replace(".",'_') for col in dataset.columns]
+    dataset = dataset.drop('fnlwgt', axis=1)
+    dataset.replace('?', np.nan, inplace=True)
+    
+    dataset.dropna(inplace=True)
+    
+    features = dataset.drop('income', axis=1)
+    target = dataset['income']
+    
+    features = pd.get_dummies(features)
+    target = target.map({
+        '<=50K': 0,
+        '>50K': 1
+    })
+    
+    return train_test_split(features, target, test_size = 0.2, stratify = target)
