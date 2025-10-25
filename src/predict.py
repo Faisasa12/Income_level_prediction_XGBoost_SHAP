aa94994736bdcaca51d83bf70d93ce4ab79d09e1
Faisasa12 <faisalmufadhi@gmail.com>
2025-10-25 17:18:37 +0100

    Added SHAP explanations to predict.py

--- src/predict.py
+++ src/predict.py
@@ -3,11 +3,13 @@ import numpy as np
 import xgboost as xgb  
 import argparse
 from preprocess import processed_data_from_input
+import shap
 
 
 parser = argparse.ArgumentParser(description="Predict income using trained model")
 parser.add_argument('--input', type=str, required=True, help="CSV file path")
 parser.add_argument('--model', type=str, required=True, help="Saved model path")
+parser.add_argument('--explain', action='store_true', help='Add SHAP explanation')
 
 args = parser.parse_args()
 
@@ -30,3 +32,13 @@ results['prediction'] = preds
 results['probability'] = probs
 
 print(results)
+
+if args.explain:
+    print("Generating SHAP explanation for input...")
+
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(input)
+
+    shap.summary_plot(shap_values, input)
+    shap.summary_plot(shap_values, input, plot_type="bar")
+      
